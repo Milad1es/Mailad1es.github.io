cd955c77eaa77d65dcee825ddfe775812733da37
Milad Akrami <milakr@elevad.umea.se>
2023-11-17 10:20:19 +0100

    klr med photo app

---
+++ docs/_app/immutable/chunks/10-8dfbc368.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/photoapp/_page.svelte-d43256d7.js";export{t as component};
---
+++ src/routes/photoapp/+page.svelte
@@ -0,0 +1,28 @@
+
+<title>Photo App</title> /*Titeln som visas vanligtvis i webbläsarens
+titelfält/fliknamn*/
+<h1 align="center">Photo App</h1>
+
+<hr/>
+<div>
+<img src="https://s3.us-west-1.amazonaws.com/esdelatino.com/wp-content/uploads/2023/05/04002718/La-busqueda-cancelada-de-Vampire-Academy-Show-para-un-nuevo.jpg" alt="myImage"  >
+<hr/>
+
+</div>
+
+<h2 align="center">Vad är vampyr?</h2>
+<a href="https://sv.wikipedia.org/wiki/Vampyr">Vampyer</a>
+<hr/>
+
+<style>
+h1{color: rgb(0, 106, 199);
+font-size: 30px;
+}
+
+h2{color: crimson;
+}
+a{font-size: 15px;
+color: aliceblue;
+}
+
+</style>
